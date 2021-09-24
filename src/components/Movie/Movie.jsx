4da226f5dfed4f57f9d950cfed9d9da424661492
Fabiola Suárez <81818021+fabiolajsm@users.noreply.github.com>
2021-09-24 18:34:58 -0300

    alert 2.0.1

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -7,6 +7,7 @@ import styles from './Movie.module.css';
 export default function Movie({ movie }) {
     const dispatch = useDispatch();
     const [alert, setAlert] = useState(false);
+    const [message, setMessage] = useState("");
 
     return (
         <div className={styles.row} key={movie.imdbID}>
@@ -14,18 +15,20 @@ export default function Movie({ movie }) {
             <img className={styles.img} src={movie.Poster.length > 7 ? movie.Poster : "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"} alt="" />
             <div className={styles.buttons}>
                 <button type="button" className="btn btn-outline-secondary" name="watch" onClick={() => {
+                    setMessage("Watch")
                     setAlert(true)
                     dispatch(addWatch(movie))
                 }} > Add Watch </button>
                 <button type="button" className="btn btn-outline-secondary" name="watched" onClick={() => {
+                    setMessage("Watched")
                     setAlert(true)
                     dispatch(addWatched(movie))
                 }} > Add Watched </button>
             </div>
             {alert ?
-                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    <strong>{movie.Title}</strong> , was added
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close" onClick={() => setAlert(false)}>
+                <div className="alert alert-success alert fade show" role="alert">
+                    <strong>{movie.Title}</strong>, added to "{message}"
+                    <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={() => setAlert(false)}>
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -16,11 +16,11 @@ function rootReducer(state = initialState, action) {
                 moviesLoaded: action.payload.Search
             };
         case ADD_WATCH:
-            return { ...state, watch: state.watch.concat(action.payload) };
+            if (!state.watch.includes(action.payload)) return { ...state, watch: state.watch.concat(action.payload) };
         case REMOVE_WATCH:
             return { ...state, watch: state.watch.filter((movie) => movie.id !== action.payload) }
         case ADD_WATCHED:
-            return { ...state, watched: state.watched.concat(action.payload) };
+            if (state.watched.includes(action.payload)) return { ...state, watched: state.watched.concat(action.payload) };
         case REMOVE_WATCHED:
             return { ...state, watched: state.watched.filter((movie) => movie.id !== action.payload) }
         case GET_MOVIE_DETAIL:
