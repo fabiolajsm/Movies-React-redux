d8363d339fefa354efbdf62fa6ceb00efcfe82ad
Fabiola Suárez <81818021+fabiolajsm@users.noreply.github.com>
2021-09-28 11:27:26 -0300

    remove funcional

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -13,24 +13,38 @@ export default function Movie({ movie, param }) {
         <div className={styles.row} key={movie.imdbID}>
             <h3 className={styles.title}>{movie.Title}</h3>
             <img className={styles.img} src={movie.Poster.length > 7 ? movie.Poster : "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"} alt="" />
-            {!param ?
+            {param === "watch" ?
                 <div className={styles.buttons}>
-                    <button type="button" className="btn btn-outline-secondary" name="watch" onClick={() => {
+                    <button type="button" className="btn btn-outline-secondary" name="removeWatch" onClick={() => {
                         setMessage("Watch")
                         setAlert(true)
-                        dispatch(addWatch(movie))
-                    }} > Add Watch </button>
-                    <button type="button" className="btn btn-outline-secondary" name="watched" onClick={() => {
+                        dispatch(removeWatch(movie.imdbID))
+                    }} > Remove from Watch </button>
+                </div>
+                : param === "watched" ? <div className={styles.buttons}>
+                    <button type="button" className="btn btn-outline-secondary" name="removeWatch" onClick={() => {
                         setMessage("Watched")
                         setAlert(true)
-                        dispatch(addWatched(movie))
-                    }} > Add Watched </button>
+                        dispatch(removeWatched(movie.imdbID))
+                    }} > Remove from Watched </button>
                 </div>
-                : null
+                    :
+                    <div className={styles.buttons}>
+                        <button type="button" className="btn btn-outline-secondary" name="watch" onClick={() => {
+                            setMessage("Watch")
+                            setAlert(true)
+                            dispatch(addWatch(movie))
+                        }} > Add Watch </button>
+                        <button type="button" className="btn btn-outline-secondary" name="watched" onClick={() => {
+                            setMessage("Watched")
+                            setAlert(true)
+                            dispatch(addWatched(movie))
+                        }} > Add Watched </button>
+                    </div>
             }
             {alert ?
                 <div className="alert alert-success alert fade show" role="alert">
-                    <strong>{movie.Title}</strong>, added to "{message}"
+                    <strong>{movie.Title}</strong>, {!param ? "added to" : "removed from"} "{message}"
                     <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={() => setAlert(false)}>
                         <span aria-hidden="true">&times;</span>
                     </button>
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -18,13 +18,13 @@ function rootReducer(state = initialState, action) {
         case ADD_WATCH:
             if (!state.watch.includes(action.payload)) return { ...state, watch: state.watch.concat(action.payload) };
             break;
-        case REMOVE_WATCH:
-            return { ...state, watch: state.watch.filter((movie) => movie.id !== action.payload) }
         case ADD_WATCHED:
             if (!state.watched.includes(action.payload)) return { ...state, watched: state.watched.concat(action.payload) };
             break;
+        case REMOVE_WATCH:
+            return { ...state, watch: state.watch.filter((movie) => movie.imdbID !== action.payload) }
         case REMOVE_WATCHED:
-            return { ...state, watched: state.watched.filter((movie) => movie.id !== action.payload) }
+            return { ...state, watched: state.watched.filter((movie) => movie.imdbID !== action.payload) }
         case GET_MOVIE_DETAIL:
             return { ...state, movieDetail: action.payload }
         default:
