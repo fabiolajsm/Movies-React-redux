15bcb2d84b2967478b263c8ff08de0df1476373b
Fabiola Suárez <81818021+fabiolajsm@users.noreply.github.com>
2021-09-28 23:07:13 -0300

    space/around

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -17,7 +17,7 @@ const Search = () => {
         <>
             <form className="form-inline my-2 my-lg-0" onSubmit={handleSubmit}>
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" value={title} onChange={(e) => setTitle(e.target.value)} />
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onSubmit={handleSubmit}>Search</button>
+                <button className="btn btn-outline-secondary" type="submit" onSubmit={handleSubmit}>Search</button>
             </form>
         </>
     );
